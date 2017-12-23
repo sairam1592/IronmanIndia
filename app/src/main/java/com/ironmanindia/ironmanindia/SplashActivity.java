257ae266a4147f5728121807f3bf22fef06b70ac
Arun <sairam.a1592@gmail.com>
2017-12-23 13:50:08 +0800

    ironman india initial commit & Android project configuration

---
+++ app/src/main/java/com/ironmanindia/ironmanindia/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.ironmanindia.ironmanindia;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mindvalley on 23/12/2017.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
